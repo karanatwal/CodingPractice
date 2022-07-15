734b4ae92ebd6725fcb3bcb714cbb9d38e1c112a
karandeepatwal <karandeep.atwal@carrier.com>
2022-07-15 15:30:58 +0530

    Initial commit

---
+++ app/src/main/java/codingpractice/65.kt
@@ -0,0 +1,20 @@
+package codingpractice
+
+fun main() {
+
+}
+
+fun isNumber(s: String): Boolean {
+
+    return false
+}
+
+fun isDecimal(s: String) : Boolean {
+
+    return false
+}
+
+fun isInteger(s: String) : Boolean {
+
+    return false
+}
