126cde6ef7ddddac357b90dd00464e3bae9bc8fd
karandeepatwal <karandeep.atwal@carrier.com>
2024-12-11 00:35:20 +0530

    1st commit

---
+++ app/src/main/java/codingpractice/random/TripletsWithSumZero.kt
@@ -0,0 +1,25 @@
+package codingpractice.random
+
+/*
+Find all triplets with zero sum
+https://www.geeksforgeeks.org/find-triplets-array-whose-sum-equal-zero/
+
+Given an array of distinct elements. The task is to find triplets in the array whose sum is zero.
+
+Input: arr[] = {0, -1, 2, -3, 1}
+Output: (0 -1 1), (2 -3 1)
+Explanation: The triplets with zero sum are 0 + -1 + 1 = 0 and 2 + -3 + 1 = 0
+
+Input: arr[] = {1, -2, 1, 0, 5}
+Output: 1 -2  1
+Explanation: The triplets with zero sum is 1 + -2 + 1 = 0
+*/
+
+fun main() {
+
+}
+
+fun findTriplet(array: IntArray = intArrayOf(0, -1, 2, -3, 1)) {
+
+}
+
